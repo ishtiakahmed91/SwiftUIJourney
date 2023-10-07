4bf6efe8f5b00944a62f09c92f06fea876f6268f
Ishtiak Ahmed <ishtiakahmed91@gmail.com>
2023-10-08 00:28:11 +0200

    - Folder structure and initial items.

---
+++ SwiftUIJourney/Scene/HomeView.swift
@@ -0,0 +1,22 @@
+//
+//  HomeView.swift
+//  SwiftUIJourney
+//
+//  Created by Ishtiak Ahmed
+//  LinkedIn: https://www.linkedin.com/in/ishtiakahmed/
+//  Twitter: https://twitter.com/ishtiz_
+//
+
+import SwiftUI
+import SwiftData
+
+struct HomeView: View {
+    var body: some View {
+        NavigationStack{
+        }
+    }
+}
+
+#Preview {
+    HomeView()
+}
---
+++ SwiftUIJourney/SwiftUIJourneyApp.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIJourneyApp.swift
+//  SwiftUIJourney
+// 
+//  Created by Ishtiak Ahmed
+//  LinkedIn: https://www.linkedin.com/in/ishtiakahmed/
+//  Twitter: https://twitter.com/ishtiz_
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SwiftUIJourneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
